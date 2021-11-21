f5d3bd905b95154bf1fbb2900688175b1651b29d
César Nogueira <github@cesarnogueira.com>
2021-11-21 22:14:39 +0000

    added intermediate state

--- catkin_ws/src/wall_following_robot/scripts/follow_wall.py
+++ catkin_ws/src/wall_following_robot/scripts/follow_wall.py
@@ -18,17 +18,20 @@ g_side = 0
 g_alpha = 0
 
 g_linear_speed = 0
-g_factor = 1
 
 g_state = 0
 
 g_turn_start_time = 0
+
+g_wall_direction = None
+
 
 def update_command_vel(linear_vel, angular_vel):
     msg = Twist()
     msg.linear.x = linear_vel
     msg.angular.z = angular_vel
     g_pub.publish(msg)
+
 
 def scan_callback(msg):
     scan_max_value = msg.range_max
@@ -41,22 +44,16 @@ def scan_callback(msg):
         'left':  min(msg.ranges[90], scan_max_value),
     }
 
-    global g_side, g_alpha, g_linear_speed, g_factor, g_state, g_turn_start_time
+    global g_side, g_alpha, g_linear_speed, g_state, g_turn_start_time, g_wall_direction
 
-    if g_state == 0:
+    if g_state == 0:  # wander
         g_linear_speed = 0.1
 
-        for r in regions.values():
-            if r < 1:
+        for r, v in regions.items():
+            if v < scan_max_value:
                 g_state = 1
-                if g_side == 0:
-                    left = (regions['left'] + regions['fleft']) / 2
-                    right = (regions['right'] + regions['fright']) / 2
-                    
-                    if left < 1 or right < 1:
-                        g_side = -1 if right < left else 1
-                    else:
-                        g_side = random.randrange(-1, 2, 2)
+                g_wall_direction = r
+                g_turn_start_time = time.time()
                 return
 
         delta_time = time.time() - g_turn_start_time
@@ -64,16 +61,44 @@ def scan_callback(msg):
         if delta_time > 6:
             rand = random.randrange(0, 5)
 
+            rand = 4
+
             g_alpha = math.pi / 2 - rand * math.pi / 4
 
             g_turn_start_time = time.time()
         elif delta_time > 1:
             g_alpha = 0
-    elif g_state == 1:
-        g_factor = 1 if regions['front'] > 1 else 2.5
+    elif g_state == 1:  # drive towards wall
+        g_linear_speed = 0.1
 
-        # g_linear_speed = 0.1 if regions['front'] > 2 else regions['front'] / 2 * 0.1
+        if regions['front'] < 0.4:
+            g_state = 2
+            if g_side == 0:
+                left = (regions['left'] + regions['fleft']) / 2
+                right = (regions['right'] + regions['fright']) / 2
 
+                if left < scan_max_value or right < scan_max_value:
+                    g_side = -1 if right < left else 1
+                else:
+                    g_side = random.randrange(-1, 2, 2)
+            return
+
+        delta_time = time.time() - g_turn_start_time
+
+        if delta_time <= 1:
+            if g_wall_direction == 'left':
+                g_alpha = math.pi / 2
+            elif g_wall_direction == 'fleft':
+                g_alpha = math.pi / 4
+            elif g_wall_direction == 'front':
+                g_alpha = 0
+            elif g_wall_direction == 'fright':
+                g_alpha = -math.pi / 4
+            elif g_wall_direction == 'right':
+                g_alpha = -math.pi / 2
+        else:
+            g_alpha = 0
+    elif g_state == 2:  # follow wall
         if g_side == -1:
             y0 = regions['right']
             x1 = regions['fright'] * math.sin(math.pi / 4)
@@ -82,12 +107,14 @@ def scan_callback(msg):
             y0 = regions['left']
             x1 = regions['fleft'] * math.sin(math.pi / 4)
             y1 = regions['fleft'] * math.cos(math.pi / 4)
-        
+
         turn_fix = (0 if regions['front'] > 0.5 else 0.7 - regions['front'])
 
-        g_alpha = g_side * min(math.atan2(y1 - distance_wall, x1 + wall_lead - y0), 0.5) #- turn_fix
+        g_alpha = g_side * (math.atan2(y1 - distance_wall,
+                            x1 + wall_lead - y0) + turn_fix)
 
         print(g_alpha)
+
 
 if __name__ == '__main__':
     rospy.init_node('wall_following_robot')
